9a446f6893d95379171d470b2b293d8903a68fda
Natalio Garbus <nataliogarbus@gmail.com>
2025-12-28 09:49:57 -0300

    fix: rename globals.css to main.css to force cache bust

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
-import "./globals.css";
+import "./main.css";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const outfit = Outfit({ subsets: ["latin"], variable: '--font-outfit' });
