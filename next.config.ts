c0e0fc511025576671dd37988c8e562a6e4afeab
Natalio Garbus <nataliogarbus@gmail.com>
2025-12-28 09:46:04 -0300

    fix: revert output export, return to standard node build

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   compress: false,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
